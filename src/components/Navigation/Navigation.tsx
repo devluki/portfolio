f03ea1d331c2c0090c9eff2db6bc56d881881eb6
devluki <lukasz.zabiegli@gmail.com>
2024-04-15 21:11:13 +0200

    Fix issue with dislocated language toggle button in sticky nav

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -15,64 +15,66 @@ const Navigation = () => {
         });
     });
     return (
-        <nav className={`${styles.nav} ${sticky}`}>
+        <>
             <LanguageToggleBtn />
-            <ul className={styles.nav__list}>
-                <li className={styles.nav__item}>
-                    <a href="#" className={styles.nav__logo}>
-                        <svg
-                            id="svg"
-                            className={styles.svg}
-                            viewBox="0 0 100 100"
-                        >
-                            <rect
-                                id="svgBox"
-                                fill="#f9f9f9"
-                                x="37"
-                                y="0"
-                                rx="5"
-                            />
-                            <rect
-                                id="svgBox--0"
-                                fill="#f9f9f9"
-                                x="37"
-                                y="25"
-                                rx="5"
-                            />
-                            <rect
-                                id="svgBox--1"
-                                fill="#f9f9f9"
-                                x="37"
-                                y="50"
-                                rx="5"
-                            />
-                            <rect
-                                id="svgBox--2"
-                                fill="#f9f9f9"
-                                x="63"
-                                y="50"
-                                rx="5"
-                            />
-                        </svg>
-                    </a>
-                </li>
-                <li className={styles.nav__item}>
-                    <a href="#" className={styles.nav__link}>
-                        <Translator translationKey="navigation.home" />
-                    </a>
-                </li>
-                <li className={styles.nav__item}>
-                    <a href="#" className={styles.nav__link}>
-                        <Translator translationKey="navigation.projects" />
-                    </a>
-                </li>
-                <li className={styles.nav__item}>
-                    <a href="#" className={styles.nav__link}>
-                        <Translator translationKey="navigation.contact" />
-                    </a>
-                </li>
-            </ul>
-        </nav>
+            <nav className={`${styles.nav} ${sticky}`}>
+                <ul className={styles.nav__list}>
+                    <li className={styles.nav__item}>
+                        <a href="#" className={styles.nav__logo}>
+                            <svg
+                                id="svg"
+                                className={styles.svg}
+                                viewBox="0 0 100 100"
+                            >
+                                <rect
+                                    id="svgBox"
+                                    fill="#f9f9f9"
+                                    x="37"
+                                    y="0"
+                                    rx="5"
+                                />
+                                <rect
+                                    id="svgBox--0"
+                                    fill="#f9f9f9"
+                                    x="37"
+                                    y="25"
+                                    rx="5"
+                                />
+                                <rect
+                                    id="svgBox--1"
+                                    fill="#f9f9f9"
+                                    x="37"
+                                    y="50"
+                                    rx="5"
+                                />
+                                <rect
+                                    id="svgBox--2"
+                                    fill="#f9f9f9"
+                                    x="63"
+                                    y="50"
+                                    rx="5"
+                                />
+                            </svg>
+                        </a>
+                    </li>
+                    <li className={styles.nav__item}>
+                        <a href="#" className={styles.nav__link}>
+                            <Translator translationKey="navigation.home" />
+                        </a>
+                    </li>
+                    <li className={styles.nav__item}>
+                        <a href="#" className={styles.nav__link}>
+                            <Translator translationKey="navigation.projects" />
+                        </a>
+                    </li>
+                    <li className={styles.nav__item}>
+                        <a href="#" className={styles.nav__link}>
+                            <Translator translationKey="navigation.contact" />
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </>
     );
 };
 
